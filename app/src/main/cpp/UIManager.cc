927fed3c50c99170e412669cf592b14aad6addc9
bcuixart <bernatcuixart@gmail.com>
2025-07-29 17:52:03 +0200

    Improved UI

--- app/src/main/cpp/UIManager.cc
+++ app/src/main/cpp/UIManager.cc
@@ -14,13 +14,21 @@ void UIManager::Render_MainMenu(const int screenWidth, const int screenHeight, c
 void UIManager::Render_Playing(const int screenWidth, const int screenHeight, const float deltaTime)
 {
     char buff[100];
-    sprintf(buff, "Coins: %d", GameManager::instance->GetCoinsCurrent());
+    sprintf(buff, "Stars: %d", GameManager::instance->GetCoinsCurrent());
     DrawText(buff, screenWidth / 2, 0, 25, WHITE);
 }
 
 void UIManager::Render_PlayerDead(const int screenWidth, const int screenHeight, const float deltaTime)
 {
+    char buff[100];
+    sprintf(buff, "Stars: %d", GameManager::instance->GetCoinsTotal());
+    DrawText(buff, screenWidth / 2, 0, 25, WHITE);
 
+    sprintf(buff, "Stars: %d", GameManager::instance->GetCoinsCurrent());
+    DrawText(buff, screenWidth / 2, screenHeight / 2, 25, WHITE);
+
+    sprintf(buff, "Highscore: %d", GameManager::instance->GetCoinsHighscore());
+    DrawText(buff, screenWidth / 2, screenHeight / 2 - 50, 25, WHITE);
 }
 
 void UIManager::Render(const int screenWidth, const int screenHeight, const float deltaTime)
